8eb8a3db136c1a5cf7dd83cc0688ce4fb96bc67f
Beamanator <dabeamanator@gmail.com>
2019-06-21 22:52:29 +0200

    L9 - setting up new app for hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SongList() {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>This wild darkness</li>
+                <li>Memory gospel</li>
+            </ul>
+        </div>
+    );
+}
